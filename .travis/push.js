7778cc66db3c9eaa7776cc4a44a51cad4f476b20
Travis CI <travis@travis-ci.org>
2018-01-24 13:27:18 -0500

    Update writeups with Travis.

---
+++ .travis/push.js
@@ -0,0 +1,14 @@
+#!/bin/sh
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git remote add origin "https://${GH_TOKEN}@github.com/northerncoalition/northerncoalition.github.io.git" > /dev/null 2>&1
+
+git diff --quiet HEAD
+if [ $? -ne 0 ]; then
+  git add 'data/writeups.json'
+  git commit --message "[Travis] Update data/writeups.json file."
+  git push --quiet origin master
+else
+  echo 'Writeups not changed.'
+fi
