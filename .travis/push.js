c5f9d52c03ddf099e19ad343085e2f8abf9c0e28
Travis CI <travis@travis-ci.org>
2018-01-24 13:49:34 -0500

    Update .travis/push.js file.

--- .travis/push.js
+++ .travis/push.js
@@ -6,7 +6,9 @@ git remote add origin "https://${GH_TOKEN}@github.com/northerncoalition/northern
 
 git diff --quiet HEAD
 if [ $? -ne 0 ]; then
-  git add 'data/writeups.json'
+  echo 'Writeups changed.'
+  git add data/writeups.json
+  git status
   git commit --message "[Travis] Update data/writeups.json file."
   git push --quiet origin master
 else
